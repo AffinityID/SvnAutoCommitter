f0d9731a1d2310353fbe66444c944f3ee7d45679
Andrey Shchekin <ashmind@gmail.com>
2014-03-19 14:35:16 +1300

    Simplified AssemblyInfo and added license.

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -5,14 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SvnAutoCommitter Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("AFFINITY ID LTD")]
+[assembly: AssemblyCompany("Affinity ID ")]
 [assembly: AssemblyProduct("SvnAutoCommitter.Tests")]
-[assembly: AssemblyCopyright("Copyright © AFFINITY ID LTD 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
